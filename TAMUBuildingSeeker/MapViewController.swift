bb1fdc72c2d10cb328d7836f4c705640cc8710e9
AlexanderHallCS <hallale22howdy@tamu.edu>
2021-10-14 11:38:07 -0500

    Major refactoring - Move ML code to a new table view VC, add buttons in right places, add UI picker to select building

--- TAMUBuildingSeeker/MapViewController.swift
+++ TAMUBuildingSeeker/MapViewController.swift
@@ -7,52 +7,102 @@
 
 import UIKit
 import MapKit
+import Vision
+import CoreML
+import ImageIO
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet var takePictureButton: UIButton!
     
     let manager = CLLocationManager()
     
-    let buildingCoordinates: [String: (CLLocationCoordinate2D)] = ["BSBW":CLLocationCoordinate2D(latitude: 30.61567,longitude: -96.33946)]
+    let buildingCoordinates: [String: (CLLocationCoordinate2D)] = ["BSBW":CLLocationCoordinate2D(latitude: 30.61567,longitude: -96.33946), "PETR":CLLocationCoordinate2D(latitude: 30.6159816,longitude: -96.338583),"SCC":CLLocationCoordinate2D(latitude: 30.6158783,longitude: -96.3400321),"RDER":CLLocationCoordinate2D(latitude: 30.6128318,longitude: -96.3424932)]
+    
+    var selectedBuilding = ""
+    
+    let request = MKDirections.Request()
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: UpdatedV2ThirteenCampusBuildings().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    var classificationResult: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        takePictureButton.backgroundColor = #colorLiteral(red: 0.3058823529, green: 0.04705882353, blue: 0.03921568627, alpha: 1)
+        
         mapView.mapType = .mutedStandard
         self.mapView.delegate = self
+        
         manager.delegate = self
         manager.requestLocation()
         manager.startUpdatingLocation()
         
         let buildingMarker = MKPointAnnotation()
-        buildingMarker.coordinate = buildingCoordinates["BSBW"]!
-        buildingMarker.title = "Biological Sciences Building West"
+        switch(selectedBuilding) {
+        case "Student Computing Center":
+            buildingMarker.coordinate = buildingCoordinates["SCC"]!
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: buildingCoordinates["SCC"]!))
+        case "Biological Sciences Building West":
+            buildingMarker.coordinate = buildingCoordinates["BSBW"]!
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: buildingCoordinates["BSBW"]!))
+        case "Peterson Building":
+            buildingMarker.coordinate = buildingCoordinates["PETR"]!
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: buildingCoordinates["PETR"]!))
+        case "Rudder Tower":
+            buildingMarker.coordinate = buildingCoordinates["RDER"]!
+            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: buildingCoordinates["RDER"]!))
+        default:
+            break
+        }
+        
+        buildingMarker.title = selectedBuilding
         mapView.addAnnotation(buildingMarker)
         
         mapView.showsUserLocation = true
-    }
-    // TODO: Request permission to use location (rather than have it set to true in Settings app by default) --> See info.plist if it's bugging out
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            let request = MKDirections.Request()
-            request.source = MKMapItem(placemark: MKPlacemark(coordinate: location.coordinate))
-            request.destination = MKMapItem(placemark: MKPlacemark(coordinate: buildingCoordinates["BSBW"]!))
-            
-            request.requestsAlternateRoutes = true
-            request.transportType = .walking
+        
+        request.requestsAlternateRoutes = true
+        request.transportType = .walking
+        
+        
+        let directions = MKDirections(request: request)
 
-            let directions = MKDirections(request: request)
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
 
-            directions.calculate { [unowned self] response, error in
-                guard let unwrappedResponse = response else { return }
-
-                for route in unwrappedResponse.routes {
-                    mapView.addOverlay(route.polyline)
-                    mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
-                }
+            for route in unwrappedResponse.routes {
+                mapView.addOverlay(route.polyline)
+                mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
             }
         }
     }
+    
+    
+    // TODO: Request permission to use location (rather than have it set to true in Settings app by default) --> See info.plist if it's bugging out
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    /*func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            //request.source = MKMapItem(placemark: MKPlacemark(coordinate: location.coordinate))
+
+            
+        }
+    } */
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to fetch user's location: \(error.localizedDescription)")
@@ -62,6 +112,112 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
         renderer.strokeColor = UIColor.blue
         return renderer
+    }
+    
+    @IBAction func takePicture(_ sender: UIButton) {
+        // Show options for the source picker only if the camera is available.
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        let photoSourcePicker = UIAlertController()
+        let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .camera)
+        }
+        let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        
+        photoSourcePicker.addAction(takePhoto)
+        photoSourcePicker.addAction(choosePhoto)
+        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(photoSourcePicker, animated: true)
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
+    
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                // unable to classify image
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+        
+            if classifications.isEmpty {
+                self.classificationResult = []
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(3)
+                let descriptions = topClassifications.map { classification in
+                    // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
+                   return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                }
+                self.classificationResult = descriptions
+                self.performSegue(withIdentifier: "mapToTable", sender: nil)
+                print("DESCRIPTIONS: " + descriptions.description)
+            }
+        }
+    }
+    
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        //resultLabel.text = "Classifying..."
+        
+        var orientation: CGImagePropertyOrientation = .down
+        switch image.imageOrientation {
+        case .up:
+            orientation = .up
+        case .right:
+            orientation = .right
+        case .down:
+            orientation = .down
+        case .left:
+            orientation = .left
+        default:
+            orientation = .down
+        }
+        
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: CGImagePropertyOrientation(rawValue: orientation.rawValue)! ,options: [:])
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        var capturedImage = UIImage()
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            capturedImage = pickedImage
+        }
+        updateClassifications(for: capturedImage)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? ResultsTableViewController {
+            vc.percentages = classificationResult
+        }
     }
 
 }
--- TAMUBuildingSeeker/StartViewController.swift
+++ TAMUBuildingSeeker/StartViewController.swift
@@ -6,132 +6,15 @@
 //
 
 import UIKit
-import Vision
-import CoreML
-import ImageIO
 
-class StartViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class StartViewController: UIViewController {
     
-    @IBOutlet var resultLabel: UILabel!
-    
-    lazy var classificationRequest: VNCoreMLRequest = {
-        do {
-            let model = try VNCoreMLModel(for: UpdatedV2ThirteenCampusBuildings().model)
-            
-            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
-                self?.processClassifications(for: request, error: error)
-            })
-            request.imageCropAndScaleOption = .centerCrop
-            return request
-        } catch {
-            fatalError("Failed to load Vision ML model: \(error)")
-        }
-    }()
+    @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startButton.backgroundColor = #colorLiteral(red: 0.3058823529, green: 0.04705882353, blue: 0.03921568627, alpha: 1)
     }
-
-    @IBAction func takePicture(_ sender: UIButton) {
-        // Show options for the source picker only if the camera is available.
-        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
-            presentPhotoPicker(sourceType: .photoLibrary)
-            return
-        }
-        
-        let photoSourcePicker = UIAlertController()
-        let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
-            self.presentPhotoPicker(sourceType: .camera)
-        }
-        let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) { [unowned self] _ in
-            self.presentPhotoPicker(sourceType: .photoLibrary)
-        }
-        
-        photoSourcePicker.addAction(takePhoto)
-        photoSourcePicker.addAction(choosePhoto)
-        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        present(photoSourcePicker, animated: true)
-    }
-    
-    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
-        let picker = UIImagePickerController()
-        picker.allowsEditing = false
-        picker.delegate = self
-        picker.sourceType = sourceType
-        present(picker, animated: true)
-    }
-    
-    func processClassifications(for request: VNRequest, error: Error?) {
-        DispatchQueue.main.async {
-            self.resultLabel.textColor = .red
-            guard let results = request.results else {
-                self.resultLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
-                return
-            }
-            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
-            let classifications = results as! [VNClassificationObservation]
-        
-            if classifications.isEmpty {
-                self.resultLabel.text = "Nothing recognized."
-            } else {
-                // Display top classifications ranked by confidence in the UI.
-                let topClassifications = classifications.prefix(6)
-                let descriptions = topClassifications.map { classification in
-                    // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
-                   return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
-                }
-                print("DESCRIPTIONS: " + descriptions.description)
-                self.resultLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
-            }
-        }
-    }
-    
-    /// - Tag: PerformRequests
-    func updateClassifications(for image: UIImage) {
-        resultLabel.text = "Classifying..."
-        
-        var orientation: CGImagePropertyOrientation = .down
-        switch image.imageOrientation {
-        case .up:
-            orientation = .up
-        case .right:
-            orientation = .right
-        case .down:
-            orientation = .down
-        case .left:
-            orientation = .left
-        default:
-            orientation = .down
-        }
-        
-        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: CGImagePropertyOrientation(rawValue: orientation.rawValue)! ,options: [:])
-            do {
-                try handler.perform([self.classificationRequest])
-            } catch {
-                /*
-                 This handler catches general image processing errors. The `classificationRequest`'s
-                 completion handler `processClassifications(_:error:)` catches errors specific
-                 to processing that request.
-                 */
-                print("Failed to perform classification.\n\(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true)
-        
-        var capturedImage = UIImage()
-        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            capturedImage = pickedImage
-        }
-        updateClassifications(for: capturedImage)
-    }
-    
 }
 
