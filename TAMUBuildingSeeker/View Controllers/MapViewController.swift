14fa1a2e95c50e4d98a68942da9ec074d5d8d25b
AlexanderHallCS <hallale21@yahoo.com>
2022-01-09 22:56:52 -0600

    Request location permissions. Remove survey, results table, and survey VCs. Comment out old maps code.

--- TAMUBuildingSeeker/View Controllers/MapViewController.swift
+++ TAMUBuildingSeeker/View Controllers/MapViewController.swift
@@ -24,14 +24,16 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     let manager = CLLocationManager()
     
-    let buildingCoordinates: [String: (CLLocationCoordinate2D)] = ["ANNEX_LIBR":CLLocationCoordinate2D(latitude: 30.6163635,longitude: -96.33843075),"BSBW":CLLocationCoordinate2D(latitude: 30.61561355,longitude: -96.33967812),"BTLR":CLLocationCoordinate2D(latitude: 30.61484579,longitude: -96.33893333),"EABAA":CLLocationCoordinate2D(latitude: 30.61593593,longitude: -96.33709296),"EABAB":CLLocationCoordinate2D(latitude: 30.61563357,longitude: -96.33749011),"EABAC":CLLocationCoordinate2D(latitude: 30.61541765, longitude: -96.33785592),"HELD":CLLocationCoordinate2D(latitude: 30.61510356,longitude: -96.33870627),"LAAH":CLLocationCoordinate2D(latitude: 30.61760828,longitude: -96.33806657),"PAV":CLLocationCoordinate2D(latitude: 30.61684315,longitude: -96.33802255), "PETR":CLLocationCoordinate2D(latitude: 30.61602477,longitude: -96.33851940),"RDER":CLLocationCoordinate2D(latitude: 30.61274023,longitude: -96.34015579),"SBISA":CLLocationCoordinate2D(latitude: 30.61675223,longitude: -96.34382093),"SCC":CLLocationCoordinate2D(latitude: 30.61589394,longitude: -96.33785416)]
+    //let buildingCoordinates: [String: (CLLocationCoordinate2D)] = ["ANNEX_LIBR":CLLocationCoordinate2D(latitude: 30.6163635,longitude: -96.33843075),"BSBW":CLLocationCoordinate2D(latitude: 30.61561355,longitude: -96.33967812),"BTLR":CLLocationCoordinate2D(latitude: 30.61484579,longitude: -96.33893333),"EABAA":CLLocationCoordinate2D(latitude: 30.61593593,longitude: -96.33709296),"EABAB":CLLocationCoordinate2D(latitude: 30.61563357,longitude: -96.33749011),"EABAC":CLLocationCoordinate2D(latitude: 30.61541765, longitude: -96.33785592),"HELD":CLLocationCoordinate2D(latitude: 30.61510356,longitude: -96.33870627),"LAAH":CLLocationCoordinate2D(latitude: 30.61760828,longitude: -96.33806657),"PAV":CLLocationCoordinate2D(latitude: 30.61684315,longitude: -96.33802255), "PETR":CLLocationCoordinate2D(latitude: 30.61602477,longitude: -96.33851940),"RDER":CLLocationCoordinate2D(latitude: 30.61274023,longitude: -96.34015579),"SBISA":CLLocationCoordinate2D(latitude: 30.61675223,longitude: -96.34382093),"SCC":CLLocationCoordinate2D(latitude: 30.61589394,longitude: -96.33785416)]
     
     //var selectedBuilding = ""
+    let mapAnnotations: [MKPointAnnotation] = []
     
     let request = MKDirections.Request()
     
-    var classificationResult: [String] = []
+    //var classificationResult: [String] = []
     var didGetFirstLocation = false
+    
     var coordinates: [GeoPoint] = []
     
     override func viewDidLoad() {
@@ -40,10 +42,10 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         //fetchMLModelFile() // DELETE THIS LATER
         //downloadedMLModelFile()
         
-        NotificationCenter.default.addObserver(self,
-                         selector: #selector(presentImagePreviewControls),
-                         name: NSNotification.Name ("sendPreviewImage"),
-                         object: nil)
+//        NotificationCenter.default.addObserver(self,
+//                         selector: #selector(presentImagePreviewControls),
+//                         name: NSNotification.Name ("sendPreviewImage"),
+//                         object: nil)
         
         //takePictureButton.backgroundColor = #colorLiteral(red: 0.3058823529, green: 0.04705882353, blue: 0.03921568627, alpha: 1)
         
@@ -51,35 +53,40 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         self.mapView.delegate = self
         
         manager.delegate = self
-        manager.requestLocation()
-        manager.startUpdatingLocation()
-        
-        let coordinates = [CLLocationCoordinate2D(latitude: 30.6132970, longitude: -96.3409940), CLLocationCoordinate2D(latitude: 30.614635239430765, longitude: -96.33918907598739),CLLocationCoordinate2D(latitude: 30.614321299604413, longitude: -96.33882430116313),CLLocationCoordinate2D(latitude: 30.615392337642014, longitude: -96.33720420138674),CLLocationCoordinate2D(latitude: 30.614810655204593, longitude: -96.33809472574791),CLLocationCoordinate2D(latitude: 30.616177215386124, longitude: -96.33937144185),CLLocationCoordinate2D(latitude: 30.615743252104618, longitude: -96.34011174809874),CLLocationCoordinate2D(latitude: 30.61573401860394, longitude: -96.3400902902438),CLLocationCoordinate2D(latitude: 30.616288023445797, longitude: -96.34070184020244),CLLocationCoordinate2D(latitude: 30.616204922997778, longitude: -96.3409378787194),CLLocationCoordinate2D(latitude: 30.61633419024504, longitude: -96.34109881435528),CLLocationCoordinate2D(latitude: 30.61620492238295, longitude: -96.3412812072978),CLLocationCoordinate2D(latitude: 30.616297255938285, longitude: -96.34137776902726),CLLocationCoordinate2D(latitude: 30.615235418417686, longitude: -96.34109881128131),CLLocationCoordinate2D(latitude: 30.614939950952216, longitude: -96.34083058876958),CLLocationCoordinate2D(latitude: 30.614302848825638, longitude: -96.34093787685902),CLLocationCoordinate2D(latitude: 30.613998147779352, longitude: -96.34086277522175),CLLocationCoordinate2D(latitude: 30.613665746287364, longitude: -96.34105589232338),CLLocationCoordinate2D(latitude: 30.61347184557696, longitude: -96.34091641869196),CLLocationCoordinate2D(latitude: 30.6132970, longitude: -96.3409940)]
-        
-        let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
-        mapView.addOverlay(polyline)
+        manager.requestAlwaysAuthorization()
+        //manager.requestLocation()
+        //request.requestsAlternateRoutes = true
+        request.transportType = .walking
         
         
-        var regionRect = polyline.boundingMapRect
-
-
-                var wPadding = regionRect.size.width * 0.25
-                var hPadding = regionRect.size.height * 0.25
-
-                //Add padding to the region
-                regionRect.size.width += wPadding
-                regionRect.size.height += hPadding
-
-                //Center the region on the line
-                regionRect.origin.x -= wPadding / 2
-                regionRect.origin.y -= hPadding / 2
-
-        mapView.setRegion(MKCoordinateRegion(regionRect), animated: true)
+        //let coordinates = [CLLocationCoordinate2D(latitude: 30.6132970, longitude: -96.3409940), CLLocationCoordinate2D(latitude: 30.614635239430765, longitude: -96.33918907598739),CLLocationCoordinate2D(latitude: 30.614321299604413, longitude: -96.33882430116313),CLLocationCoordinate2D(latitude: 30.615392337642014, longitude: -96.33720420138674),CLLocationCoordinate2D(latitude: 30.614810655204593, longitude: -96.33809472574791),CLLocationCoordinate2D(latitude: 30.616177215386124, longitude: -96.33937144185),CLLocationCoordinate2D(latitude: 30.615743252104618, longitude: -96.34011174809874),CLLocationCoordinate2D(latitude: 30.61573401860394, longitude: -96.3400902902438),CLLocationCoordinate2D(latitude: 30.616288023445797, longitude: -96.34070184020244),CLLocationCoordinate2D(latitude: 30.616204922997778, longitude: -96.3409378787194),CLLocationCoordinate2D(latitude: 30.61633419024504, longitude: -96.34109881435528),CLLocationCoordinate2D(latitude: 30.61620492238295, longitude: -96.3412812072978),CLLocationCoordinate2D(latitude: 30.616297255938285, longitude: -96.34137776902726),CLLocationCoordinate2D(latitude: 30.615235418417686, longitude: -96.34109881128131),CLLocationCoordinate2D(latitude: 30.614939950952216, longitude: -96.34083058876958),CLLocationCoordinate2D(latitude: 30.614302848825638, longitude: -96.34093787685902),CLLocationCoordinate2D(latitude: 30.613998147779352, longitude: -96.34086277522175),CLLocationCoordinate2D(latitude: 30.613665746287364, longitude: -96.34105589232338),CLLocationCoordinate2D(latitude: 30.61347184557696, longitude: -96.34091641869196),CLLocationCoordinate2D(latitude: 30.6132970, longitude: -96.3409940)]
         
-        let startEndMarker = MKPointAnnotation()
-        startEndMarker.coordinate = CLLocationCoordinate2D(latitude: 30.6132970, longitude: -96.3409940)
-        startEndMarker.title = "Start/End"
-        mapView.addAnnotation(startEndMarker)
+       // let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+        //mapView.addOverlay(polyline)
+        
+        
+//        var regionRect = polyline.boundingMapRect
+//
+//
+//                var wPadding = regionRect.size.width * 0.25
+//                var hPadding = regionRect.size.height * 0.25
+//
+//                //Add padding to the region
+//                regionRect.size.width += wPadding
+//                regionRect.size.height += hPadding
+//
+//                //Center the region on the line
+//                regionRect.origin.x -= wPadding / 2
+//                regionRect.origin.y -= hPadding / 2
+
+        //mapView.setRegion(MKCoordinateRegion(regionRect), animated: true)
+        
+//        let startEndMarker = MKPointAnnotation()
+//        startEndMarker.coordinate = CLLocationCoordinate2D(latitude: 30.6132970, longitude: -96.3409940)
+//        startEndMarker.title = "Start/End"
+        //mapView.addAnnotation(startEndMarker)
+        
+        
         //let buildingMarker = MKPointAnnotation()
 //        switch(selectedBuilding) {
 //        case "Evans Library Annex":
@@ -128,8 +135,22 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         //buildingMarker.title = selectedBuilding
         //mapView.addAnnotation(buildingMarker)
         
-        request.requestsAlternateRoutes = true
-        request.transportType = .walking
+       
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedAlways:
+            fallthrough
+        case .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        default:
+            let alert = UIAlertController(title: "Error", message: "Please enable location tracking in the Settings app!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default) {(action: UIAlertAction) -> Void in
+                alert.removeFromParent()
+            })
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     
     @IBAction func presentCameraView(_ sender: UIButton) {
@@ -249,6 +270,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         print("BING CHILLING 9")
     }
     
+    // TODO: DISPLAY NOTIFICATION IF RESULT IS SUCCESSFUL OR NOT OR NEEDS ANOTHER PICTURE
     func processClassifications(for request: VNRequest, error: Error?) {
         DispatchQueue.main.async {
             guard let results = request.results else {
@@ -257,7 +279,11 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             }
             let classifications = results as! [VNClassificationObservation]
         
-            if classifications.isEmpty {
+            let topClassifications = classifications.prefix(3)
+            let descriptions = topClassifications.map { classification in
+               return String(format: "  %.2f %@", classification.confidence, classification.identifier)
+            }
+            /*if classifications.isEmpty {
                 self.classificationResult = []
             } else {
                 // Get top 3 classifications
@@ -287,8 +313,18 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                 } */
                 self.performSegue(withIdentifier: "mapToTable", sender: nil)
                 //print("DESCRIPTIONS: " + descriptions.description)
-            }
+            } */
         }
+    }
+    
+    private func showDestinationAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default) {(action: UIAlertAction) -> Void in
+            alert.removeFromParent()
+            // TODO: ADD ROUTE TO MAP WITH MARKER (r)
+        })
+        self.present(alert, animated: true, completion: nil)
     }
     
     @objc func confirmButtonPressed() {
@@ -302,38 +338,38 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         }
     }
     
-    func renameResult(result: String) -> String {
-        switch result {
-        case "ANNEX_LIBR":
-            return "Evans Library Annex"
-        case "BSBW":
-            return "Biological Sciences Building West"
-        case "BTLR":
-            return "Butler Hall"
-        case "EABAA":
-            return "Engineering Activity Building A"
-        case "EABAB":
-            return "Engineering Activity Building B"
-        case "EABAC":
-            return "Engineering Activity Building C"
-        case "HELD":
-            return "Heldenfelds Hall"
-        case "LAAH":
-            return "Liberal Arts & Humanities Building"
-        case "PAV":
-            return "Pavilion"
-        case "PETR":
-            return "Peterson Building"
-        case "RDER":
-            return "Rudder Tower"
-        case "SBISA":
-            return "SBISA Dining Hall"
-        case "SCC":
-            return "Student Computing Center"
-        default:
-            return result
-        }
-    }
+//    func renameResult(result: String) -> String {
+//        switch result {
+//        case "ANNEX_LIBR":
+//            return "Evans Library Annex"
+//        case "BSBW":
+//            return "Biological Sciences Building West"
+//        case "BTLR":
+//            return "Butler Hall"
+//        case "EABAA":
+//            return "Engineering Activity Building A"
+//        case "EABAB":
+//            return "Engineering Activity Building B"
+//        case "EABAC":
+//            return "Engineering Activity Building C"
+//        case "HELD":
+//            return "Heldenfelds Hall"
+//        case "LAAH":
+//            return "Liberal Arts & Humanities Building"
+//        case "PAV":
+//            return "Pavilion"
+//        case "PETR":
+//            return "Peterson Building"
+//        case "RDER":
+//            return "Rudder Tower"
+//        case "SBISA":
+//            return "SBISA Dining Hall"
+//        case "SCC":
+//            return "Student Computing Center"
+//        default:
+//            return result
+//        }
+//    }
     
     /// - Tag: PerformRequests
     func updateClassifications(for image: UIImage) {
@@ -402,11 +438,11 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     } */
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let vc = segue.destination as? UINavigationController {
+        /*if let vc = segue.destination as? UINavigationController {
             if let childVC = vc.viewControllers[0] as? ResultsTableViewController {
                 childVC.percentages = classificationResult
             }
-        }
+        } */
     }
 
     @IBAction func unwindToMap(segue: UIStoryboardSegue) {}
